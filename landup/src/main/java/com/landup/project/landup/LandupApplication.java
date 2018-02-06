7a4357b07bf1fe4b7ec3b973c937c31151bbfbdf
banik <bani@banikui-MacBook-Pro.local>
2018-02-06 12:44:12 +0900

    Initial commit

---
+++ landup/src/main/java/com/landup/project/landup/LandupApplication.java
@@ -0,0 +1,12 @@
+package com.landup.project.landup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandupApplication.class, args);
+	}
+}
---
+++ landup/src/test/java/com/landup/project/landup/LandupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.landup.project.landup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LandupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
